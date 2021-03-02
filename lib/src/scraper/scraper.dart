99b609f635b155fb8a3342e6beece6f0af070f6e
OmarMalass <Omar1Malass@gmail.com>
2021-03-03 01:10:29 +0200

    Update base Scraper class. - Added template variable class.

--- lib/src/scraper/scraper.dart
+++ lib/src/scraper/scraper.dart
@@ -1,13 +1,27 @@
 import 'package:html/dom.dart';
+import 'package:http/http.dart';
+import 'package:html/parser.dart' as html_parser;
+import 'package:vokabularium_scraper/src/options/options.dart';
 
 import 'package:vokabularium_scraper/src/page/page.dart';
 import 'package:vokabularium_scraper/src/parser/parser.dart';
 import 'package:vokabularium_scraper/src/scrap/scrap.dart';
 
-abstract class Scraper<T extends Page> {
+abstract class Scraper<T extends Page, O extends Options<T>> {
+  static var client = Client();
+
+  String buildURL(O options);
+
+  Future<Scrap<T>> scrap(O options);
+
   Parser<T> getParser();
 
-  Scrap<T> scrap(Document document) {
-    return getParser().parse(document);
+  Future<Document> fetchDocument(O options) => fetch(buildURL(options));
+
+  Future<Document> fetch(String url) async {
+    var response = await client.get(url);
+    return html_parser.parse(response.body);
   }
+
+  Scrap<T> scrapDocument(Document document) => getParser().parse(document);
 }
